60afa3722ea362efd3192520ef036df16e17d58d
Xinyu Li <xinyul3@andrew.cmu.edu>
2026-03-30 11:18:31 -0400

    Adjust disparity parameters in run_yolo_disparity.sh

--- run_yolo_disparity.sh
+++ run_yolo_disparity.sh
@@ -14,15 +14,15 @@ exec python3 yolo_raspi_cam.py \
   --hfov-deg 100 \
   --vfov-deg 67.5 \
   --stereo-algo sgbm \
-  --disp-num 48 \
-  --disp-block-size 5 \
-  --stereo-proc-scale 0.5 \
+  --disp-num 64 \
+  --disp-block-size 9 \
+  --stereo-proc-scale 0.4 \
   --disp-every 2 \
   --min-depth-m 0.20 \
   --max-depth-m 3.5 \
   --depth-scale 1.875 \
   --depth-offset 0.4375 \
-  --imgsz 320 \
+  --imgsz 256 \
   --conf 0.35 \
   --show \
   --show-disparity \
