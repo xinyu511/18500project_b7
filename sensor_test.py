e4b45ede2e2deed4b32d067e1dbb94801674d43f
wuyanglll <wuyang.robin.li@gmail.com>
2026-04-17 17:57:47 -0400

    corrected sensor test

--- sensor_test.py
+++ sensor_test.py
@@ -1,35 +1,12 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import DistanceSensor
+from time import sleep
 
-TRIG = 23
-ECHO = 24
+sensor = DistanceSensor(
+    echo=24,     # GPIO24
+    trigger=23,  # GPIO23
+    max_distance=2  # meters
+)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-def get_distance():
-    GPIO.output(TRIG, False)
-    time.sleep(0.05)
-
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
-
-    duration = pulse_end - pulse_start
-    distance = duration * 17150  # cm
-    return round(distance, 2)
-
-try:
-    while True:
-        print("Distance:", get_distance(), "cm")
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    print("Distance:", round(sensor.distance * 100, 2), "cm")
+    sleep(0.5)
